005baf5a13a08ff3c74b22dd1ed2c14bd3923f27
gsolvit <752622384@qq.com>
2019-12-23 12:12:07 +0800

    Vgg

--- CNN/Vgg/vgg.py
+++ CNN/Vgg/vgg.py
@@ -8,8 +8,7 @@ except ImportError:
 
 
 __all__ = [
-    'VGG', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn',
-    'vgg19_bn', 'vgg19',
+    'VGG', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn', 'vgg19_bn', 'vgg19',
 ]
 
 
