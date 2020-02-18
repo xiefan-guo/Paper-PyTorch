eb7c6ed5dd563fabfc573a1f861adc53bdb244b6
gsolvit <752622384@qq.com>
2020-02-18 22:59:16 +0800

    EdgeConnect

--- Image Inpainting/EdgeConnect Generative Image Inpainting with Adversarial Edge Learning/src/util.py
+++ Image Inpainting/EdgeConnect Generative Image Inpainting with Adversarial Edge Learning/src/util.py
@@ -18,5 +18,19 @@ def create_mask(width, height, mask_width, mask_height, x=None, y=None):
     return mask
 
 
+def create_dir(dir):
+
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+
+def stitch_image(inputs, *outputs, img_per_row=2):
+
+    gap = 5
+    columns = len(outputs) + 1
+
+    width, height = inputs[0][:, :, 0].shape
+    img = Image.new('RGB', (width * img_per_row * columns + gap * (img_per_row - 1), height * int(len(inputs) / img_per_row)))
+
 
 
